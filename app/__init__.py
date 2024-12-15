58afddc088a5da833e26d69bfa7ded81f1603d76
Kiran Khaire <khairekiran2@gmail.com>
2024-12-15 16:21:17 +0530

    CORS enabled

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from app.auth.models import UserModule  # Import User class from auth.models
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
+    CORS(app, resources={r"/auth/*": {"origins": "*"}})
     
     jwt = JWTManager(app)
 
